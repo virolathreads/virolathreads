6aab337350a00a0f47a879d213801bd0a9b1dcf4
Moyosore <moyosorejames@gmail.com>
2024-11-26 23:22:28 +0100

    loader

--- src/lib/PreLoader.jsx
+++ src/lib/PreLoader.jsx
@@ -7,7 +7,10 @@ export default function PreLoader() {
         <div class="preloader-inner position-relative">
           <div class="preloader-circle"></div>
           <div class="preloader-img pere-text">
-            <img src="https://res-console.cloudinary.com/dd0mdsb3h/media_explorer_thumbnails/264a416c430c2c1f3c94e36e220dd334/detailed" alt="" />
+            <img
+              src="https://res.cloudinary.com/dd0mdsb3h/image/upload/v1731457982/ef3tnweirfwsvvd1djwc.png"
+              alt=""
+            />
           </div>
         </div>
       </div>
